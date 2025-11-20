059064d59b24f1bd7b224bceb32de99d305485ce
R.Dávid <98745267+PiciMadar@users.noreply.github.com>
2025-11-20 14:00:17 +0100

    Add files via upload

---
+++ Sombrero kifőzde/Sombrero kifőzde/CsirkesEtel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sombrero_kifőzde
+{
+    public class CsirkesEtel : Etel
+    {
+        public string Hus {  get; set; }
+        public CsirkesEtel(string nev, string tortilla, int ar, string hus) : base(nev, tortilla, ar)
+        {
+            Hus = hus;
+        }
+    }
+}
---
+++ Sombrero kifőzde/Sombrero kifőzde/DisznosEtel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sombrero_kifőzde
+{
+    public class DisznosEtel : Etel
+    {
+        public string Hus {  get; set; }
+        public DisznosEtel(string nev, string tortilla, int ar, string hus) : base(nev, tortilla, ar)
+        {
+            Hus = hus;
+        }
+    }
+}
