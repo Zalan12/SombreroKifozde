369714ef1dc274b0bb1bb5c68bdf300b0d17f6df
R.Dávid <98745267+PiciMadar@users.noreply.github.com>
2025-11-27 11:19:40 +0100

    Kiiratás és beolvasás elkezdése

---
+++ SombreroKifozde/Sombrero kifőzde/Sombrero kifőzde/AlkoholosItal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sombrero_kifőzde
+{
+    public abstract class AlkoholosItal : Ital
+    {
+        public double Alkohol_tartalom {  get; set; }
+        protected AlkoholosItal(string nev, int ar, double alkohol_tartalom) : base(nev, ar)
+        {
+            Alkohol_tartalom = alkohol_tartalom;
+        }
+    }
+}
